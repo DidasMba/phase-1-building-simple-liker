76dd82cbff979d99a8d2423f134b9cb42f7c4ead
DidasMba <daniel.mbarushi@gmail.com>
2023-10-05 00:23:12 +0300

    complet like

--- main.js
+++ main.js
@@ -5,7 +5,45 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+});
 
+const emptyHearts = document.querySelectorAll(".like-glyph");
+
+emptyHearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        // Change the heart to a full heart
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      })
+      .catch((error) => {
+        // Display the error modal
+        const errorModal = document.getElementById("modal");
+        const errorMessage = document.getElementById("modal-message");
+        errorMessage.innerText = error;
+        errorModal.classList.remove("hidden");
+
+        // Hide the modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
+
+const fullHearts = document.querySelectorAll(".activated-heart");
+
+fullHearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    // Change the heart back to an empty heart
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove("activated-heart");
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
